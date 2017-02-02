9f34a62470eb59f559f7acde46cad8498b06c2a1
Waseem999 <wakarim2@gmail.com>
2017-02-02 13:59:16 -0600

    more debugging

--- app/tests/containers/ProfileContainer.spec.jsx
+++ app/tests/containers/ProfileContainer.spec.jsx
@@ -2,3 +2,18 @@ import React from 'react';
 import chai, { expect, assert } from 'chai';
 import { render, shallow } from 'enzyme';
 import ProfileContainer from '../../containers/ProfileContainer';
+
+chai.use(require('chai-enzyme')());
+
+describe ('<ProfileContainer />', () => {
+    let wrapper, profile; 
+})
+
+beforeEach(() => {
+    wrapper = shallow(<ProfileContainer />)
+})
+
+it('should run without errors', () => {
+    assert(render(<ProfileContainer />));
+})
+
